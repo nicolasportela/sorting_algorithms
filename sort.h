2cdd089bc640f55912e589d16ecbab65377e14e3
nicolasportela <2103@holbertonschool.com>
2021-02-03 02:20:17 +0000

    task 1 and header file updated

--- sort.h
+++ sort.h
@@ -23,6 +23,8 @@ void quick_sort(int *array, size_t size);
 int _split(int *array, int first, int last, size_t size);
 void quick_sort_parts(int *array, int first, int last, size_t size);
 void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **head, listint_t *node, listint_t *next_node);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
