f8b61b95d46ad9802c52e206313d2eb318edd92b
nicolasportela <2103@holbertonschool.com>
2021-07-02 21:14:58 +0000

    fixes

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,35 +11,35 @@
  */
 void shell_sort(int *array, size_t size)
 {
-  int left, right, knuth, aux, flag;
+	int left, right, knuth, aux, flag;
 
-  if (array == NULL || size < 2)
-    return;
+	if (array == NULL || size < 2)
+		return;
 
-  knuth = 1;
-  while (knuth <= (int)size / 3)
-    {
-      knuth = knuth * 3 + 1;
-    }
-
-  while (knuth > 0)
-    {
-      flag = 1;
-      for (left = 0; left < (int)size - knuth; left++)
+	knuth = 1;
+	while (knuth <= (int)size / 3)
 	{
-	  right = left + knuth;
-	  if (array[left] > array[right])
-	    {
-	      aux = array[left];
-	      array[left] = array[right];
-	      array[right] = aux;
-	      flag = 0;
-	    }
+		knuth = knuth * 3 + 1;
 	}
-      if (flag == 1)
+
+	while (knuth > 0)
 	{
-	  knuth = (knuth - 1) / 3;
-	  print_array(array, size);
+		flag = 1;
+		for (left = 0; left < (int)size - knuth; left++)
+		{
+			right = left + knuth;
+			if (array[left] > array[right])
+			{
+				aux = array[left];
+				array[left] = array[right];
+				array[right] = aux;
+				flag = 0;
+			}
+		}
+		if (flag == 1)
+		{
+			knuth = (knuth - 1) / 3;
+			print_array(array, size);
+		}
 	}
-    }
 }
--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,16 +7,16 @@
  */
 void swap_node(listint_t **list, listint_t *node)
 {
-  node->next->prev = node->prev;
-  if (node->prev)
-    node->prev->next = node->next;
-  else
-    *list = node->next;
-  node->prev = node->next;
-  node->next = node->next->next;
-  node->prev->next = node;
-  if (node->next)
-    node->next->prev = node;
+	node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		*list = node->next;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
 }
 
 /**
@@ -27,39 +27,39 @@ void swap_node(listint_t **list, listint_t *node)
  */
 void cocktail_sort_list(listint_t **list)
 {
-  static int swapped = 1;
-  listint_t *tmp;
+	static int swapped = 1;
+	listint_t *tmp;
 
-  if (list == NULL || *list == NULL)
-    return;
-  tmp = *list;
-  while (swapped != 0)
-    {
-      swapped = 0;
-      while (tmp->next != NULL)
+	if (list == NULL || *list == NULL)
+		return;
+	tmp = *list;
+	while (swapped != 0)
 	{
-	  if (tmp->next->n < tmp->n)
-	    {
-	      swap_node(list, tmp);
-	      swapped = 1;
-	      print_list(*list);
-	    }
-	  else
-	    tmp = tmp->next;
+		swapped = 0;
+		while (tmp->next != NULL)
+		{
+			if (tmp->next->n < tmp->n)
+			{
+				swap_node(list, tmp);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				tmp = tmp->next;
+		}
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (tmp->prev != NULL)
+		{
+			if (tmp->prev->n > tmp->n)
+			{
+				swap_node(list, tmp->prev);
+				swapped = 1;
+				print_list(*list);
+			}
+			else
+				tmp = tmp->prev;
+		}
 	}
-      if (swapped == 0)
-	break;
-      swapped = 0;
-      while (tmp->prev != NULL)
-	{
-	  if (tmp->prev->n > tmp->n)
-	    {
-	      swap_node(list, tmp->prev);
-	      swapped = 1;
-	      print_list(*list);
-	    }
-	  else
-	    tmp = tmp->prev;
-	}
-    }
 }
